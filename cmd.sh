61c6b4195396558e46252a923a9e3be6d84a635f
dishant_wsl <dishant.raut@nymbleup.com>
2024-12-07 06:37:02 +0530

    feat(auth) : custom user model step 1

--- cmd.sh
+++ cmd.sh
@@ -7,8 +7,12 @@ docker-compose up
 docker-compose up -d
 docker-compose run --rm app sh -c "flake8"
 docker-compose run --rm app sh -c "python manage.py test"
+
 docker-compose run --rm app sh -c "django-admin startapp core"
 docker-compose run --rm app sh -c "python manage.py wait_for_db"
 docker-compose run --rm app sh -c "django-admin startproject app ."
+docker-compose run --rm app sh -c "python manage.py makemigrations"
 docker-compose run --rm app sh -c "python manage.py runserver 0.0.0.0:8005"
+docker-compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"
+
 
